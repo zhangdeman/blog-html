458ec9ab74f1292cbb64b9e5d3309f28652a833a
张德满 <2215508028@qq.com>
2017-12-28 22:20:10 +0800

    从服务端拉取开发者信息

--- js/web-js/developer.js
+++ js/web-js/developer.js
@@ -1,18 +1,19 @@
 var developer = {
 
     init : function () {
-
+        this.getDevelopInfo()
     },
 
     getDevelopInfo : function () {
         $.ajax({
             type: 'GET',
-            url: "/developer/getDevelopInfo",
+            url: requestUrl+"/developer/getDevelopInfo",
             data: {},
             dataType: "json",
             success: function (data) {
                 console.log(data);
                 developInfo = data.data;
+                developer.setDevelopInfo(developInfo);
             },
             error : function () {
 
@@ -21,7 +22,21 @@ var developer = {
     },
 
     setDevelopInfo : function (developerInfo) {
-
+        var htmlContent = "<h1>我的名片</h1>\n" +
+            "<p>姓名："+developerInfo.real_name+"</p>" +
+            "      <p>网名："+developerInfo.nick_name+"</p>\n" +
+            "      <p>职业："+developerInfo.job+"</p>\n" +
+            "      <p>电话："+developerInfo.tel+"</p>\n" +
+            "      <p>QQ："+developerInfo.qq+"</p>\n" +
+            "      <p>Email："+developerInfo.mail+"</p>\n" +
+            "      <!--ul class=\"linkmore\">\n" +
+            "        <li><a href=\"#\" class=\"talk\" title=\"给我留言\"></a></li>\n" +
+            "        <li><a href=\"#\" class=\"address\" title=\"联系地址\"></a></li>\n" +
+            "        <li><a href=\"#\" class=\"email\" title=\"给我写信\"></a></li>\n" +
+            "        <li><a href=\"#\" class=\"photos\" title=\"生活照片\"></a></li>\n" +
+            "        <li><a href=\"#\" class=\"heart\" title=\"关注我\"></a></li>\n" +
+            "      </ul-->";
+        $("#developer-info").html(htmlContent);
     }
 };
 
