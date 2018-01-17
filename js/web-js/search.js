95ab87d7d0a7fb51cd451d5b1c53a3b0e637059f
zhangdeman <zhangdeman@didichuxing.com>
2018-01-17 18:52:58 +0800

    初始化检索

---
+++ js/web-js/search.js
@@ -0,0 +1,19 @@
+/**
+ * 数据检索js
+ */
+var search = {
+    /**
+     * 获取请求参数
+     * @private
+     */
+    _getParams : function () {
+        
+    },
+
+    /**
+     * 执行检索
+     */
+    runSearch : function () {
+
+    }
+};
